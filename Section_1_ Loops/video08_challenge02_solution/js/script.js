3819e00987d7bb393318f61950e07398c19c6126
Dionuta Burton <diounta@gmail.com>
2018-12-23 17:36:41 -0500

    comments added

--- Section_1_ Loops/video08_challenge02_solution/js/script.js
+++ Section_1_ Loops/video08_challenge02_solution/js/script.js
@@ -1,10 +1,12 @@
 var html = '';
 var rgbColor;
 
+// makes random numbers
 function randomRGB() {
  return Math.floor(Math.random() * 256 ); 
 }
 
+//random color
 function randomColor() {
   var color = 'rgb(';
   color += randomRGB() + ',';
@@ -13,10 +15,12 @@ function randomColor() {
   return color;
 }
 
+//prints to screen
 function print(message) {
   document.write(message);
 }
 
+//the for loop that pull everything togther
 for (var i = 0; i < 100; i += 1) {
   rgbColor = randomColor();
   html += '<div style="background-color:' + rgbColor + '"></div>';
