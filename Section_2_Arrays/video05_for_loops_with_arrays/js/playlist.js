f7a7d05dbfa3e9df82eb9f4c8e2481ef16dfcd72
Dionuta Burton <diounta@gmail.com>
2018-12-24 18:31:55 -0500

    for loops done

--- Section_2_Arrays/video05_for_loops_with_arrays/js/playlist.js
+++ Section_2_Arrays/video05_for_loops_with_arrays/js/playlist.js
@@ -7,7 +7,25 @@ var playList = [
   'Maybellene'
 ];
 
+
 function print(message) {
   document.write(message);
 }
+
+// Make a list from array that is passed througth it.
+function printList (list) {
+   var listHTML = '<ol>';
+  for ( var i = 0; i < list.length; i += 1) {
+     listHTML += '<li>' + list[i] +  '</li>';
+     }
+   
+    listHTML += '</ol>';
+     print(listHTML);
+}
+
+
+
+printList(playList);
+
+
 
