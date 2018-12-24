b5ca2505757db140c3e165fd0521310ec63e890d
Dionuta Burton <diounta@gmail.com>
2018-12-23 21:27:39 -0500

    finshed arra video 2

--- Section_2_Arrays/video02_accessing_array_items/js/shopping.js
+++ Section_2_Arrays/video02_accessing_array_items/js/shopping.js
@@ -1,1 +1,10 @@
 var shopping = [ 'carrots', 'milk', 'eggs' ];
+
+// [0] = carrots
+console.log(shopping[0]);
+// [1] = milk
+console.log(shopping[1]);
+// [2] = eggs
+console.log(shopping[2]);
+// [3] = undeifined because the is not a 4th item
+console.log(shopping[3]); 
