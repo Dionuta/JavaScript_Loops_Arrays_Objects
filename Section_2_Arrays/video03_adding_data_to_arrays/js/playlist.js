ecc476a452fc9367618c96c6938ce75096d14012
Dionuta Burton <diounta@gmail.com>
2018-12-24 01:00:14 -0500

    notes added

--- Section_2_Arrays/video03_adding_data_to_arrays/js/playlist.js
+++ Section_2_Arrays/video03_adding_data_to_arrays/js/playlist.js
@@ -1,7 +1,13 @@
+//empty array
 var playList = [];
 
+// adds data to the end of an array
 playList.push(' I Did It My Way');
 playList.push('Bound 2', 'Day and Night');
+
+//adds data to the beging 
 playList.unshift('Drip To Hard', 'Too Much');
+
+// calls funtion from helper.js 
 
 printList(playList);
