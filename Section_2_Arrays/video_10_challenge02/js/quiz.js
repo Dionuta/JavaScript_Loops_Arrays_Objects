6c23af9f234cdf93366c4d439e36871521faeabf
Dionuta <diounta@gmail.com>
2018-12-26 11:24:46 -0800

    challenge passed

--- Section_2_Arrays/video_10_challenge02/js/quiz.js
+++ Section_2_Arrays/video_10_challenge02/js/quiz.js
@@ -3,13 +3,28 @@ var questions = [
   ['How many continents are there?', 7],
   ['How many legs does an insect have?', 6]
 ];
+
+
+
 var correctAnswers = 0;
 var question;
 var answer;
 var response;
+var right = [];
+var wrong = [];
 
 function print(message) {
-  document.write(message);
+var outputDiv = document.getElementById('output');
+outputDiv.innerHTML = message;
+}
+
+function buildList( arr ) {
+  var listHTML = '<ol>';
+  for (var i = 0; i < arr.length; i += 1) {
+    listHTML += '<li>' + arr[i] + '</li>';
+  }
+  listHTML += '</ol>';
+  return(listHTML);
 }
 
 for (var i = 0; i < questions.length; i += 1) {
@@ -19,8 +34,16 @@ for (var i = 0; i < questions.length; i += 1) {
   response = parseInt(response);
   if (response === answer) {
     correctAnswers += 1;
-  } 
+     right.push(question);    
+  } else {
+     wrong.push(question)
+  }
 }
 
 html = "You got " + correctAnswers + " question(s) right."
+html += "<h2> You got these questions correct: </h2>";
+html += buildList(right);
+html += "<h2> You got these question wrong:</h2>";
+html += buildList(wrong);
 print(html);
+
