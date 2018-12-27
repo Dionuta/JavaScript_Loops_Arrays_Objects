1637691830cfc726a9af00a9786300502971d644
Dionuta <diounta@gmail.com>
2018-12-26 21:47:33 -0800

    vid 06 done

---
+++ Section_3_Objects/video06_challenge_01/js/students.js
@@ -0,0 +1,8 @@
+var students = [
+  {name : 'James', track : 'CSS', Achievements : 3, points : 500 },
+  {name : 'Mike', track : 'IOS', Achievements : 35, points : 2000 },
+  {name : 'Ace', track : 'JavaScript', Achievements : 100, points : 1578 },
+  {name : 'Wayne', track : 'Full Stack', Achievements : 8211, points : 4846 },
+  {name : 'Nick', track : 'Front End', Achievements : 11, points : 846 },
+];
+
