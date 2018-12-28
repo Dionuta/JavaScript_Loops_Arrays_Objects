2e2e54c980d2b67a139558354412cce5a97df7f3
Dionuta <diounta@gmail.com>
2018-12-27 21:56:33 -0800

    last vid finshed

--- Section_3_Objects/video10_challenge03/js/student_report.js
+++ Section_3_Objects/video10_challenge03/js/student_report.js
@@ -1,15 +1,33 @@
 var message = '';
 var student;
+var search;
 
+//prints message 
 function print(message) {
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
 }
 
-for (var i = 0; i < students.length; i += 1) {
-  student = students[i];
-  message += '<h2>Student: ' + student.name + '</h2>';
-  message += '<p>Track: ' + student.track + '</p>';
-  message += '<p>Points: ' + student.points + '</p>';
-  message += '<p>Achievements: ' + student.achievements + '</p>';
+//creats a report for student 
+function getStudentReport( student ) {
+  var report = '<h2>Student: ' + student.name + '</h2>';
+  report += '<p>Track: ' + student.track + '</p>';
+  report += '<p>Points: ' + student.points + '</p>';
+  report += '<p>Achievements: ' + student.achievements + '</p>';
+  return report;
+}
+
+//search array 
+while (true) {
+  search = prompt('Search student records: type a name [Jody] (or type "quit" to end)');
+  if (search === null || search.toLowerCase() === 'quit') {
+    break;
+  }
+  for (var i = 0; i < students.length; i += 1) {
+    student = students[i];
+    if ( student.name === search ) {
+      message = getStudentReport( student );
+      print(message);
+    }
+  }
 }
