7a394e4cdfbfdfe7624e68ad5f3488dbc36312e3
Dionuta <diounta@gmail.com>
2018-12-28 22:26:41 -0800

    Notes and Let added

--- Section_1_ Loops/video07_challenge01/js/script.js
+++ Section_1_ Loops/video07_challenge01/js/script.js
@@ -1,11 +1,11 @@
-var html = ' ';
+let html = ' ';
 
-
-for (var i = 0; i <= 10; i += 1) {
-  var red = Math.floor(Math.random() * 256 );
-  var blue = Math.floor(Math.random() * 256 );
-  var green = Math.floor(Math.random() * 256 );
-  var rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+//creats a random  rgb colored div with a for loop
+for (let i = 0; i <= 10; i += 1) {
+  let red = Math.floor(Math.random() * 256 );
+  let blue = Math.floor(Math.random() * 256 );
+  let green = Math.floor(Math.random() * 256 );
+  let rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
   html += '<div style="background-color:' + rgbColor + '"></div>';
   i;
 }
