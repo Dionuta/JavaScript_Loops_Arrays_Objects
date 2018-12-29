f5dd97e879efe3e18ee9a6fe76cc70d3f2366c70
Dionuta <diounta@gmail.com>
2018-12-28 21:16:39 -0800

    added notes

--- Section_1_ Loops/video03_loop_conditions/app.js
+++ Section_1_ Loops/video03_loop_conditions/app.js
@@ -2,15 +2,15 @@ var upper = 10000;
 var randomNumber = getRandomNumber (upper);
 var guess;
 var attemps  = 0;
-
+//makes random number between 1 and upper var.
 function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
-
+//runs in till number gussed
 while (guess !== randomNumber) {
     guess = getRandomNumber(upper);
     attemps += 1;
 }
-
+//prints code to site
 document.write('<p> The random number was ' + randomNumber + '.</p>');
 document.write('<p> It took the computer ' + attemps + ' attemps to get it right, </p>');
